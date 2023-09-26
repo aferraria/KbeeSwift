775b2cc51ff2cb01d35897b145161b363ea1c129
aferraria@git.novamens.com <aferraria@novamens.com>
2023-09-26 10:02:24 -0300

    Initial Commit

---
+++ KbeeSwift/KbeeSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  KbeeSwiftApp.swift
+//  KbeeSwift
+//
+//  Created by Alejo Ferraria on 26/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct KbeeSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
